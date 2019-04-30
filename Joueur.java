3fe4f17c497ba7e0c839a01d5cd07eb0b1a4f9e4
Mohamed <43723424+Mohamedeijy@users.noreply.github.com>
2019-04-30 19:02:21 +0200

    Update Joueur.java

--- Joueur.java
+++ Joueur.java
@@ -5,5 +5,23 @@ public class Joueur{
   private String nom;
   private boolean estBlanc;
 
-  
+  public Joueur(String name, boolean couleurBlanche){
+    this.nom = name;
+    this.estBlanc = couleurBlanche;
+  }
+  public void setNom(String name){
+    this.nom = name;
+  }
+  public String getNom(){
+    return this.nom;
+  }
+  public void setEstBlanche(boolean couleurBlanche){
+    this.estBlanc = couleurBlanche;
+  }
+  public boolean getEstBlanche(){
+    return this.estBlanc;
+  }
+  public void initialiserPiece(){
+    //je dois créer les classes pour chaque type de pièce
+  }
 }
