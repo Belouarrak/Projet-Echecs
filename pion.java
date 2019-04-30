aad10ef4f3a5c900a3163a7661bbeea3768715be
Mohamed <43723424+Mohamedeijy@users.noreply.github.com>
2019-04-30 20:17:12 +0200

    Update pion.java

--- pion.java
+++ pion.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.lang.*;
 
 public class Pion extends Piece{
 
@@ -6,6 +7,15 @@ public class Pion extends Piece{
     super(color);
   }
   public boolean mouvementPossible(int departX, int departY, int arriveeX, int arriveeY){
-
+    //exception de base (va falloir créer une classe exception comme ça c'est plus facile)
+    if(departX == arriveeX && departY == arriveeY)
+      return false; //cannot move nothing
+    if(arriveeX < 0 || arriveeX > 7 || departX < 0 || departX > 7 || arriveeY < 0 || arriveeY > 7 || departY <0 || departY > 7)
+      return false;//bordures
+    //
+    if(arriveeX==departX+1 && departY==arriveeY){
+      return true;
+    }
+    return false;
   }
 }
