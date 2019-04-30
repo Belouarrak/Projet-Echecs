5b3e89b864fc459b2654bbfc6ffdfef856387bda
Mohamed <43723424+Mohamedeijy@users.noreply.github.com>
2019-04-30 19:02:56 +0200

    Update pion.java

--- pion.java
+++ pion.java
@@ -1,5 +1,11 @@
 import java.util.*;
 
 public class Pion extends Piece{
-  
+
+  public Pion(int color){
+    super(color);
+  }
+  public boolean mouvementPossible(int departX, int departY, int arriveeX, int arriveeY){
+
+  }
 }
