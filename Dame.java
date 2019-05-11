074e6346e858de380a402b8a7c8d8802ba0a21e3
Mohamed <43723424+Mohamedeijy@users.noreply.github.com>
2019-05-11 18:23:37 +0200

    méthode movePossible implémentation
    Implémentation de la méthode movePossible pour la Dame, décomposée en 8, pour chaque direction.

--- Dame.java
+++ Dame.java
@@ -7,9 +7,88 @@ public class Dame extends Piece{
     super(color);
   }
   public boolean mouvementPossible(Case[][] board, int departX, int departY, int arriveeX, int arriveeY){
-    //
-    if((Math.abs(departX-arriveeX)==Math.abs(departY-arriveeY))||(departX==arriveeX || departY==arriveeY)){
-      return true;
+    //pièce sur les diagoales ou même ligne ou même colonne
+    if((Math.abs(departX-arriveeX)==Math.abs(departY-arriveeY)) || (departX==arriveeX || departY==arriveeY)){
+      //diagonale haute-droite
+      if((arriveeX-departX)==(arriveeY-departY)){
+        //regarde si il a une pièce sur le chemin
+        for (int i=1; i<arriveeX-departX; i++){
+          if (board.getCase(departX+i, departY+i).estOccupee()){
+            return false;
+          }
+        }
+        return true;
+      }
+      //diagonale haute-gauche
+      if((arriveeX-departX)==(departY-arriveeY)){
+        //regarde si il a une pièce sur le chemin
+        for (int i=1; i<arriveeX-departX; i++){
+          if (board.getCase(departX+i, departY-i).estOccupee()){
+            return false;
+          }
+        }
+        return true;
+      }
+      //diagonale basse-droite
+      if((departX-arriveeX)==(arriveeY-departY)){
+        //regarde si il a une pièce sur le chemin
+        for (int i=1; i<departX-arriveeX; i++){
+          if (board.getCase(departX-i, departY+i).estOccupee()){
+            return false;
+          }
+        }
+        return true;
+      }
+      //diagonale basse-gauche
+      if((departX-arriveeX)==(departY-arriveeY)){
+        //regarde si il a une pièce sur le chemin
+        for (int i=1; i<departX-arriveeX; i++){
+          if (board.getCase(departX-i, departY-i).estOccupee()){
+            return false;
+          }
+        }
+        return true;
+      }
+      //ligne de droite
+      if(departX==arriveeX && arriveeY-departY>0)){
+        //regarde si il a une pièce sur le chemin
+        for (int i=1; i<arriveeY-departY; i++){
+          if (board.getCase(departX, departY+i).estOccupee()){
+            return false;
+          }
+        }
+        return true;
+      }
+      //ligne de gauche
+      if(departX==arriveeX && arriveeY-departY<0)){
+        //regarde si il a une pièce sur le chemin
+        for (int i=1; i<departY-arriveeY; i++){
+          if (board.getCase(departX, departY-i).estOccupee()){
+            return false;
+          }
+        }
+        return true;
+      }
+      //colonne du haut
+      if(departY==arriveeY && arriveeX-departX>0)){
+        //regarde si il a une pièce sur le chemin
+        for (int i=1; i<arriveeX-departX; i++){
+          if (board.getCase(departX+i, departY).estOccupee()){
+            return false;
+          }
+        }
+        return true;
+      }
+      //colonne du bas
+      if(departY==arriveeY && arriveeX-departX<0)){
+        //regarde si il a une pièce sur le chemin
+        for (int i=1; i<departX-arriveeX; i++){
+          if (board.getCase(departX-i, departY).estOccupee()){
+            return false;
+          }
+        }
+        return true;
+      }
     }
     return false;
   }
