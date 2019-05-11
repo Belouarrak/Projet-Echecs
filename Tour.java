9d6eb5029f44422775b348a5895a029650d7b4e5
Mohamed <43723424+Mohamedeijy@users.noreply.github.com>
2019-05-11 18:03:36 +0200

    movePossible() implémenté
    Implémentation de la méthode, décomposée en 4 pour chaque direction

--- Tour.java
+++ Tour.java
@@ -7,10 +7,48 @@ public class Tour extends Piece{
     super(color);
   }
   public boolean mouvementPossible(Case[][] board, int departX, int departY, int arriveeX, int arriveeY){
-    //soit même colonne, soit même ligne
+    //departX=arriveeX: même ligne; departY=arriveeY: même colonne
     if(departX==arriveeX || departY==arriveeY){
-      return true;
-    }
+      //ligne de droite
+      if(departX==arriveeX && arriveeY-departY>0)){
+        //regarde si il a une pièce sur le chemin
+        for (int i=1; i<arriveeY-departY; i++){
+          if (board.getCase(departX, departY+i).estOccupee()){
+            return false;
+          }
+        }
+        return true;
+      }
+      //ligne de gauche
+      if(departX==arriveeX && arriveeY-departY<0)){
+        //regarde si il a une pièce sur le chemin
+        for (int i=1; i<departY-arriveeY; i++){
+          if (board.getCase(departX, departY-i).estOccupee()){
+            return false;
+          }
+        }
+        return true;
+      }
+      //colonne du haut
+      if(departY==arriveeY && arriveeX-departX>0)){
+        //regarde si il a une pièce sur le chemin
+        for (int i=1; i<arriveeX-departX; i++){
+          if (board.getCase(departX+i, departY).estOccupee()){
+            return false;
+          }
+        }
+        return true;
+      }
+      //colonne du bas
+      if(departY==arriveeY && arriveeX-departX<0)){
+        //regarde si il a une pièce sur le chemin
+        for (int i=1; i<departX-arriveeX; i++){
+          if (board.getCase(departX-i, departY).estOccupee()){
+            return false;
+          }
+        }
+        return true;
+      }
     return false;
   }
   public String toString(){
