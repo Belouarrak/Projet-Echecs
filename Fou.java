143ce2b12b3b796cfc6406543c579ada992372a5
Mohamed <43723424+Mohamedeijy@users.noreply.github.com>
2019-05-11 17:37:42 +0200

    movePossible() du Fou
    Implémentation de movePossible(), la méthode a été décomposée en 4 pour chaque diagonale, et à chaque fois on regarde si il a une pièce sur le chemin de la destination.

--- Fou.java
+++ Fou.java
@@ -7,8 +7,44 @@ public class Fou extends Piece{
     super(color);
   }
   public boolean mouvementPossible(Case[][] board, int departX, int departY, int arriveeX, int arriveeY){
-    //diagonale
+    //diagonale haute-droite
+    if((arriveeX-departX)==(arriveeY-departY)){
+      //regarde si il a une pièce sur le chemin
+      for (int i=1; i<arriveeX-departX; i++){
+        if (board.getCase(departX+i, arriveeX+i).estOccupee()){
+          return false;
+        }
+      }
+      return true;
+    }
+    //diagonale haute-gauche
+    if((arriveeX-departX)==(departY-arriveeY)){
+      //regarde si il a une pièce sur le chemin
+      for (int i=1; i<arriveeX-departX; i++){
+        if (board.getCase(departX+i, arriveeX-i).estOccupee()){
+          return false;
+        }
+      }
+      return true;
+    }
+    //diagonale basse-droite
+    if((departX-arriveeX)==(arriveeY-departY)){
+      //regarde si il a une pièce sur le chemin
+      for (int i=1; i<departX-arriveeX; i++){
+        if (board.getCase(departX-i, arriveeX+i).estOccupee()){
+          return false;
+        }
+      }
+      return true;
+    }
+    //diagonale basse-gauche
     if((departX-arriveeX)==(departY-arriveeY)){
+      //regarde si il a une pièce sur le chemin
+      for (int i=1; i<departX-arriveeX; i++){
+        if (board.getCase(departX-i, arriveeX-i).estOccupee()){
+          return false;
+        }
+      }
       return true;
     }
     return false;
