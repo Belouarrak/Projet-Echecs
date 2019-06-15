103984cc1da978896eb8fa5f4cf3cbd2ddb0f2d1
Mohamed <43723424+Mohamedeijy@users.noreply.github.com>
2019-06-16 01:40:55 +0200

    sauvegarde en temps réel sur le master

--- MainTests.java
+++ MainTests.java
@@ -5,7 +5,9 @@ public class MainTests{
   public static void main(String[] args){
     try{
       Partie newGame = new Partie();
+      newGame.chargerMoves("Test checkmate.txt");
       newGame.lancerPartie();
+      System.out.println(newGame.getEchiquier().toString());
     }
     catch(Exception e){System.out.println(e);}
   }
